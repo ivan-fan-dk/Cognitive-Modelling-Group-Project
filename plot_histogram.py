4b6a3368c1b0805d5baae58fe5b981bdd276add7
Ivan Fan <ivan.fan.dk@gmail.com>
2026-09-10 09:31:57 +0200

    read from csv and plot histogram, remove and correct json ratings

--- plot_histogram.py
+++ plot_histogram.py
@@ -1,7 +1,7 @@
-from pathlib import Path
 import matplotlib.pyplot as plt
-import json
+from pathlib import Path
 
+import pandas as pd
 
 def plot_histogram(file):
     """
@@ -10,25 +10,25 @@ def plot_histogram(file):
     Parameters:
     file (str): The path to the JSON file containing the ratings.
     """
-    with open(file, "r") as f:
-        sample_ratings = json.load(f)
-        # Extract the ratings from the dictionary
-        rating_values = list(sample_ratings.values())
+    df = pd.read_csv(file)  # Read the CSV file to ensure it exists and is valid
+    # Extract the ratings from the dictionary
+    rating_values = list(df['rating'])
+    rating_values.extend(df['rating2'])
 
-        # Create a histogram
-        plt.figure(figsize=(10, 6))
-        plt.hist(rating_values, bins=range(min(rating_values), max(rating_values) + 2), edgecolor='black', alpha=0.7)
-        
-        # Add titles and labels
-        plt.title('Histogram of Image Ratings')
-        plt.xlabel('Ratings')
-        plt.ylabel('Frequency')
-        
-        # Show the plot
-        plt.grid(axis='y', alpha=0.75)
-        plt.title(f'Histogram of Ratings from {file.name}')
-        plt.show()
+    # Create a histogram
+    plt.figure(figsize=(10, 6))
+    plt.hist(rating_values, bins=range(min(rating_values), max(rating_values) + 2), edgecolor='black', alpha=0.7)
+    
+    # Add titles and labels
+    plt.title('Histogram of Image Ratings')
+    plt.xlabel('Ratings')
+    plt.ylabel('Frequency')
+    
+    # Show the plot
+    plt.grid(axis='y', alpha=0.75)
+    plt.title(f'Histogram of Ratings from {file.name}')
+    plt.savefig(f'histogram_{file.stem}.svg')
 
 if __name__ == "__main__":
-    for file in Path(__file__).parent.glob("ratings_*.json"):
+    for file in Path(__file__).parent.glob("s*.csv"):
         plot_histogram(file)
